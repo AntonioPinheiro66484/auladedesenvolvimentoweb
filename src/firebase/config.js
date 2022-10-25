a327d90a7cee51dfc6f85608f0ac1a4602403e7a
AntonioPinheiro66484 <ap66484@gmail.com>
2022-10-25 10:15:51 -0300

    atualização

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTh5Fg8-N3u0R7mxJqLTe5NlYw2VGd-7o",
+  authDomain: "auladelabdevweb.firebaseapp.com",
+  projectId: "auladelabdevweb",
+  storageBucket: "auladelabdevweb.appspot.com",
+  messagingSenderId: "764706060279",
+  appId: "1:764706060279:web:d717b91719872d7b8e20f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
